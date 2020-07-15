3f4981dcd69234dc53d3be3631a1997ea2e87c56
Alexander Smarus <bender@readdle.com>
2020-07-15 21:48:54 +0300

    SR-13225 - rename from X-02

--- SR-13225/build.cmd
+++ SR-13225/build.cmd
@@ -2,4 +2,4 @@ set SWIFTC=%SystemDrive%\Library\Developer\Toolchains\unknown-Asserts-developmen
 set SDKROOT=%SystemDrive%\Library\Developer\Platforms\Windows.platform\Developer\SDKs\Windows.sdk
 set SWIFTFLAGS=-sdk %SDKROOT% -I %SDKROOT%\usr\lib\swift -L %SDKROOT%\usr\lib\swift\windows -O -g -debug-info-format=codeview
 
-%SWIFTC% -emit-library %SWIFTFLAGS% x02.swift -o x02.dll
+%SWIFTC% -emit-library %SWIFTFLAGS% sr13225.swift -o sr13225.dll
