3a0c396a0e93974c1138a86ae1b632213d665e3e
Alexander Smarus <bender@readdle.com>
2020-08-15 20:45:32 +0300

    SR-13225 - update sample to use lld instead link

--- SR-13225/build.cmd
+++ SR-13225/build.cmd
@@ -1,5 +1,5 @@
 set SWIFTC=%SystemDrive%\Library\Developer\Toolchains\unknown-Asserts-development.xctoolchain\usr\bin\swiftc.exe
 set SDKROOT=%SystemDrive%\Library\Developer\Platforms\Windows.platform\Developer\SDKs\Windows.sdk
-set SWIFTFLAGS=-sdk %SDKROOT% -I %SDKROOT%\usr\lib\swift -L %SDKROOT%\usr\lib\swift\windows -O -g -debug-info-format=codeview
+set SWIFTFLAGS=-sdk %SDKROOT% -I %SDKROOT%\usr\lib\swift -L %SDKROOT%\usr\lib\swift\windows -O -g -debug-info-format=codeview -use-ld=lld
 
 %SWIFTC% -emit-library %SWIFTFLAGS% sr13225.swift -o sr13225.dll
